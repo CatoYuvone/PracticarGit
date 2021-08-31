faeea43d0f2eca0b69251b07de9385decf643f00
Cato <otacyuvo@gmail.com>
2021-08-31 14:20:46 -0300

    Borré la función sumar y restar y empecé con el cartón

--- src/bingo.py
+++ src/bingo.py
@@ -1,9 +1,16 @@
 
-def saludo ():
-    return "Hola"
+carton_vacio = (
+        (0,0,0,0,0,0,0,0,0),
+        (0,0,0,0,0,0,0,0,0),
+        (0,0,0,0,0,0,0,0,0)
+    )
 
-def sumar (a, b):
-    return a +b
-
-def restar(a, c):
-    return a - c
+def carton():
+    carton = (
+            (1,0,0,1,1,0,1,0,1),
+            (0,1,1,1,0,1,1,1,1),
+            (0,1,0,0,1,0,0,1,0)
+    )
+    return carton
+ 
+print(carton())
